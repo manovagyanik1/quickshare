c78ad44a7aaa532c6be83a99e9be019d785656cb
manovagyanik1 <52470178+manovagyanik1@users.noreply.github.com>
2024-12-24 01:28:57 +0530

    Update src/main.tsx [BoltSync]

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { MsalProvider } from '@azure/msal-react';
+import { msalInstance } from './config/msal';
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
   </React.StrictMode>
 );
